baa93b4637605c2348f661acd22f794f45d3d86a
Floakz <sergioamoreira@sapo.pt>
2026-05-09 21:57:01 +0100

    Insert Clarity
    -Insert Clarity to track trafic

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,13 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import ScrollToTop from './components/scrolltotop.jsx'
+import { clarity } from 'clarity-js';
 
+if (import.meta.env.PROD) {
+    clarity.start({
+        projectId: 'wol6ws4jdd'
+    });
+}
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
